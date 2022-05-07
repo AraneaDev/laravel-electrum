397c10fb4c81b35babf807400642d1676cd7ba61
Aranea Development <info@aranea-development.nl>
2022-05-07 22:23:55 +0200

    Update electrum.php

--- src/routes/electrum.php
+++ src/routes/electrum.php
@@ -10,27 +10,22 @@ if (config('electrum.web_interface.enabled', false)) {
             Route::get('status', 'AraneaDev\Electrum\App\Api\StatusController');
 
             Route::get('history', 'AraneaDev\Electrum\App\Api\HistoryController@index');
-            Route::get('history/{address}', 'AraneaDev\Electrum\App\Api\HistoryController@show')
-                ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
+            Route::get('history/{address}', 'AraneaDev\Electrum\App\Api\HistoryController@show');
             Route::get('history/tx/{txid}', 'AraneaDev\Electrum\App\Api\HistoryController@details');
 
             Route::get('addresses', 'AraneaDev\Electrum\App\Api\AddressController@index');
             Route::get('addresses/unused', 'AraneaDev\Electrum\App\Api\AddressController@unused');
-            Route::get('addresses/is_mine{address}', 'AraneaDev\Electrum\App\Api\AddressController@is_mine')
-                ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
-            Route::get('addresses/{address}', 'AraneaDev\Electrum\App\Api\AddressController@check')
-                ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
+            Route::get('addresses/is_mine{address}', 'AraneaDev\Electrum\App\Api\AddressController@is_mine');
+            Route::get('addresses/{address}', 'AraneaDev\Electrum\App\Api\AddressController@check');
 
             Route::prefix('requests')->group(function () {
                 Route::get('/', 'AraneaDev\Electrum\App\Api\RequestsController@index');
-                Route::get('{address}', 'AraneaDev\Electrum\App\Api\RequestsController@show')
-                    ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
+                Route::get('{address}', 'AraneaDev\Electrum\App\Api\RequestsController@show');
 
                 Route::post('/', 'AraneaDev\Electrum\App\Api\RequestsController@create');
 
                 Route::delete('/', 'AraneaDev\Electrum\App\Api\RequestsController@clear');
-                Route::delete('{address}', 'AraneaDev\Electrum\App\Api\RequestsController@destroy')
-                    ->where('address', '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$');
+                Route::delete('{address}', 'AraneaDev\Electrum\App\Api\RequestsController@destroy');
             });
         });
     });
